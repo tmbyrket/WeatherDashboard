00a1b318c21debaec733bc37a35bdc36bf465f81
Reese B <theresa.byrket@gmail.com>
2021-05-30 15:13:48 -0500

    js update to save info searched into localstoreage

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+//Global variable declarations
+var cityList =[];
+var cityName;
+
+//local storage functions
+initCityList ();
+initWeather ();
+
+//function to display the city entered by the user into the DOM
+function renderCities(){
+    $("#cityList").empty();
+    $("#cityInput").val("");
+
+    for (i=0; i<cityList.length, i++){
+        var a = $("<a>");
+        a.addClass("list-group-item list-group-item-action list-group-item-primary city");
+        a.attr("data-name", cityList[i]);
+        a.text(cityList[i]);
+        $("#cityList").prepend(a);
+    }
+}
+
+//function to get the city list array from local
+function initCityList() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
+
+    if (storedCities !== null) {
+        cityList = storedCities;
+    }
+
+    renderCities();
+}
+
+//function to get current city into local to display current weather when refreshed
+function initWeather() {
+    var storedWeather = JSON.parse(localStorage.getItem('currentCity'));
+
+    if (storedWeather !==null) {
+        cityName=storedWeather;
+
+        displayWeather();
+        displayFiveDayForecast();
+    }
+}
+
+//function to save city in array to lcoal
+function storeCityArray() {
+    localStorage.setItem("cities", JSON.stringify(cityList));
+}
+
+//function saves the displayed current city in to the lcoal
+function storeCurrentCity() {
+    localStorage.setItem("currentCity", JSON.stringify(cityName));
+}
+
