fe28bd6d699e176961b1067f9306da794a0a8909
Reese B <theresa.byrket@gmail.com>
2021-05-30 15:41:36 -0500

    update UV status and JSON

--- assets/script.js
+++ assets/script.js
@@ -73,3 +73,51 @@ $("#citySearchBTN").on("click", function(event){
     displayFiveDayForecast();
 });
 
+//event handler for user hitting enter after inserting search term
+$("#cityInput").keypress(function(e){
+    if(e.which == 13){
+        $("#citySearchBtn").click();
+    }
+})
+
+//this function runs the API and display city and weather info
+async function displayWeather() {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname +"&units=imperial&appid=d3b85d453bf90d469c82e650a0a3da26";
+
+    var response = await $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    console.log(response);
+
+    var currentWeatherDiv = $("<div class='card-body' id='currentWeather'>");
+    var getCurrentCity = response.name;
+    var date = new Date();
+    var val=(date.getMonth()+1)+"/"+date.getDate()+"/"+date.getFullYear();
+    var getCurrentWeatherIcon = response.weather[0].icon;
+    var displayCurrentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getCurrentWeatherIcon + "@2x.png />");
+    var currentCityEl = $("<h3 class = 'card-body'>").text(getCurrentCity+" ("+val+")");
+    currentCityEl.append(displayCurrentWeatherIcon);
+    currentWeatherDiv.append(currentCityEl);
+    var getTemp = response.main.temp.toFixed(1);
+    var tempEl = $("<p class='card-text'>").text("Temperature: "+getTemp+"° F");
+    currentWeatherDiv.append(tempEl);
+    var getHumidity = response.main.humidity;
+    var humidityEl = $("<p class='card-text'>").text("Humidity: "+getHumidity+"%");
+    currentWeatherDiv.append(humidityEl);
+    var getWindSpeed = response.wind.speed.toFixed(1);
+    var windSpeedEl = $("<p class='card-text'>").text("Wind Speed: "+getWindSpeed+" mph");
+    currentWeatherDiv.append(windSpeedEl);
+    var getLong = response.coord.lon;
+    var getLat = response.coord.lat;
+
+    var uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=d3b85d453bf90d469c82e650a0a3da26&lat="+getLat+"&lon="+getLong;
+    var uvResponse = await $.ajax({
+        url: uvURL,
+        method: "GET"
+    })
+
+    //UV index and setting color class
+
+}
